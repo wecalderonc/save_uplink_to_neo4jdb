7798dbd75e839d005f4d20cfbe6a1fe53e4d4d94
wecalderonc <wecalderonc@unal.edu.co>
2019-05-23 14:55:30 -0500

    add dotenv gem for enviroment variables

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,6 +1,8 @@
 # require 'forwardable'
 require './requires/neo4j.rb'
 require './requires/dry.rb'
+require 'dotenv/load'
+
 require './lib/errors.rb'
 require './models/thing.rb'
 require './config/neo4j_config.rb'
@@ -16,24 +18,3 @@ module Handler
   end
 end
 
-event2 =
-        {
-          params: {
-            "state": {
-              "reported": {
-                "device":"2BEE81",
-                "data":"00670430080670200001ascd",
-                "time":"1548277798",
-                "snr":"16.32",
-                "station":"146A",
-                "avgSnr":"18.47",
-                "lat":"5.0",
-                "lng":"-74.0",
-                "rssi":"-111.00",
-                "seqNumber":"77"
-              }
-            }
-          }
-        }
-context = {}
-Handler.lambda_handler(event: event2, context: context)
