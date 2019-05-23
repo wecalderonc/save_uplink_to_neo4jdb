a030e28234030ed6b68be891031877a9cf6e35bb
wecalderonc <wecalderonc@unal.edu.co>
2019-05-23 15:36:32 -0500

    upcase variables

--- config/neo4j_config.rb
+++ config/neo4j_config.rb
@@ -1,4 +1,4 @@
-url = "http://#{ENV['neo4j_username']}:#{ENV['neo4j_password']}@#{ENV['neo4j_server']}"
+url = "http://#{ENV['NEO4J_USERNAME']}:#{ENV['NEO4J_PASSWORD']}@#{ENV['NEO4J_SERVER']}"
 http_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new(url)
 neo4j_session = Neo4j::Core::CypherSession.new(http_adaptor)
 Neo4j::ActiveBase.current_session = neo4j_session
