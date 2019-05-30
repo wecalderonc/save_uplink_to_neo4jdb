46d04eed7d4d3f72280be41ff28ab9f5a83c6f9c
wecalderonc <wecalderonc@unal.edu.co>
2019-05-30 13:53:29 -0500

    fix comments and fixed process when no accumulator message

--- lib/rebuild.rb
+++ lib/rebuild.rb
@@ -1,17 +1,17 @@
 module Rebuild
   def self.messages_array(messages)
-    #seleccionamos los dos mensajes que inicien por 1 o 2 los cuales se relacionan con accumulator
     accumulator_messages = messages.select { |message| message =~ /^(1|2)/ }
-    #limpiamos el array original para que no tenga esos mensajes de accumulator
-    new_messages_array = (messages+accumulator_messages)-(messages&accumulator_messages)
-    #unimos los mensajes de accumulator con el orden de bytes de mayor a menor importancia y quitamos un character
-    accumulator_messages_joined = join_accumulator_messages(accumulator_messages)
-    #agregamos nuevo string a array limpiado
-    new_messages_array.push(accumulator_messages_joined)
+    if accumulator_messages.empty?
+      messages
+    else
+      new_messages_array = (messages + accumulator_messages) - (messages & accumulator_messages)
+      accumulator_messages_joined = join_accumulator_messages(accumulator_messages)
+      new_messages_array.push(accumulator_messages_joined)
+    end
   end
 
   def self.join_accumulator_messages(payload)
-    new_payload = payload.sort_by { |t| t[0] }.reverse.join
+    new_payload = payload.sort_by { |message| message[0] }.reverse.join
     new_payload.slice!(5)
     new_payload
   end
--- spec/lib/rebuild_spec.rb
+++ spec/lib/rebuild_spec.rb
@@ -1,13 +1,38 @@
 require './lib/rebuild.rb'
 
 RSpec.describe Rebuild do
-  let(:subject) { described_class }
+  let(:subject)   { described_class }
   let(:messages)  { ["1ascd", "20000", "5aedf", "8adfd"] }
+  let(:messages2) { ["2aaaa", "8ffff", "1cccc", "3bbbb"] }
+  let(:messages3) { ["3ascd", "40000", "5aedf", "8adfd"] }
+  let(:messages4) { ["6dddd", "70000", "3aedf", "4adfd"] }
+
 
   describe "#messages_array" do
     it "Should return new array rebuild" do
       response = subject.messages_array(messages)
       expected_response = ["5aedf", "8adfd", "20000ascd"]
+
+      expect(response).to eq(expected_response)
+    end
+
+    it "Should return new array rebuild" do
+      response = subject.messages_array(messages2)
+      expected_response = ["8ffff", "3bbbb", "2aaaacccc"]
+
+      expect(response).to eq(expected_response)
+    end
+
+    it "Should return new array rebuild" do
+      response = subject.messages_array(messages3)
+      expected_response = messages3
+
+      expect(response).to eq(expected_response)
+    end
+
+    it "Should return new array rebuild" do
+      response = subject.messages_array(messages4)
+      expected_response = messages4
 
       expect(response).to eq(expected_response)
     end
@@ -17,6 +42,20 @@ RSpec.describe Rebuild do
     it "Should return a string joined with the accumulator messages" do
       response = subject.join_accumulator_messages(messages[0..1])
       expected_response = "20000ascd"
+
+      expect(response).to eq(expected_response)
+    end
+
+    it "Should return a string joined with the accumulator messages" do
+      response = subject.join_accumulator_messages(messages2[0..1])
+      expected_response = "8ffffaaaa"
+
+      expect(response).to eq(expected_response)
+    end
+
+    it "Should return a string joined with the accumulator messages" do
+      response = subject.join_accumulator_messages(messages3[0..1])
+      expected_response = "40000ascd"
 
       expect(response).to eq(expected_response)
     end
