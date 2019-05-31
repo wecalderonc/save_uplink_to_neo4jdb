07b2282a5551a0a538fd2d78ebcf3928e6b2d029
wecalderonc <wecalderonc@unal.edu.co>
2019-05-31 17:15:45 -0500

    added to docker-compose two commands line for prepare local neo4j for rspec

--- config/neo4j_config.rb
+++ config/neo4j_config.rb
@@ -1,4 +1,4 @@
-p url = "http://#{ENV['NEO4J_USERNAME']}:#{ENV['NEO4J_PASSWORD']}@#{ENV['NEO4J_SERVER']}"
+url = "http://#{ENV['NEO4J_USERNAME']}:#{ENV['NEO4J_PASSWORD']}@#{ENV['NEO4J_SERVER']}"
 http_adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new(url)
 neo4j_session = Neo4j::Core::CypherSession.new(http_adaptor)
 Neo4j::ActiveBase.current_session = neo4j_session
