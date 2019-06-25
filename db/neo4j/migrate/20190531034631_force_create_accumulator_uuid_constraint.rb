a7fb72ee76c41237f5a5f7bb4b59c5cfcd09fe46
wecalderonc <wecalderonc@unal.edu.co>
2019-06-25 16:31:09 -0500

    deleted duplicated migrations

--- db/neo4j/migrate/20190531034631_force_create_accumulator_uuid_constraint.rb
+++
@@ -1,9 +0,0 @@
-class ForceCreateAccumulatorUuidConstraint < Neo4j::Migrations::Base
-  def up
-    add_constraint :Accumulator, :uuid, force: true
-  end
-
-  def down
-    drop_constraint :Accumulator, :uuid
-  end
-end
