65d4ea5a79ab26e3f0bbfed59b669927083ab7e7
Jeisson Moreno <jmoreno@procibernetica.com>
2019-09-23 11:18:52 -0500

    refactor of services maths

--- app/services/alarms/maths.rb
+++ app/services/alarms/maths.rb
@@ -1,32 +1,47 @@
-require './app/services/alarms/maths.rb'
-
 module Alarms::Maths
   LIMIT = 10000
+  BASE_ALARMS =  {
+    unexpected_dump: false,
+    imposible_consumption: false
+  }
 
   def self.equations(last_accumulator, current_accumulator, flow_per_minute)
-    base_alarms = {
-      unexpected_dump: false,
-      imposible_consumption: false
-    }
+    last_acc = get_accumulator_value(last_accumulator)
+    current_acc = get_accumulator_value(current_accumulator)
+    delta_time = get_delta_time(current_accumulator, last_accumulator)
 
-    last_acc = last_accumulator.value.to_i(16)
-    current_acc = current_accumulator.value.to_i(16)
-    time_1 = Time.at current_accumulator.uplink.time.to_i
-    time_2 = Time.at last_accumulator.uplink.time.to_i
-    delta_time = (time_1 - time_2) / 60
+    condition_1 = (current_acc - last_acc) / delta_time
+    condition_2 = (LIMIT - last_acc + current_acc) / delta_time
 
-    main_condition = (current_acc - last_acc) / delta_time
-    main_condition_2 = (LIMIT - last_acc + current_acc) / delta_time
-
-    if (current_acc < last_acc) && (main_condition_2 > flow_per_minute)
-      base_alarms[:unexpected_dump] = true
-      base_alarms[:imposible_consumption] = true
+    if (current_acc < last_acc) && (condition_2 > flow_per_minute)
+      BASE_ALARMS[:unexpected_dump] = true
+      BASE_ALARMS[:imposible_consumption] = true
     end
 
-    if (current_acc > last_acc) && (main_condition > flow_per_minute)
-      base_alarms[:imposible_consumption] = true
+    if (current_acc > last_acc) && (condition_1 > flow_per_minute)
+      BASE_ALARMS[:imposible_consumption] = true
     end
 
-    base_alarms
+    BASE_ALARMS
+  end
+
+  private
+
+  def self.get_accumulator_value(accumulator)
+    accumulator.value.to_i(16)
+  end
+
+  def self.accumulator_time(accumulator)
+    Time.at accumulator.uplink.time.to_i
+  end
+
+  def self.get_delta_time(current_accumulator, last_accumulator)
+    time_1 = accumulator_time(current_accumulator)
+    time_2 = accumulator_time(last_accumulator)
+    (time_1 - time_2) / 60
+  end
+
+  def self.accumulator_time(accumulator)
+    Time.at accumulator.uplink.time.to_i
   end
 end
