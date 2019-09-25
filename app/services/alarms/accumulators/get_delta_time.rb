d7399982261820fde9496a7c19a8a0ca9b6ed1a5
morenojey-90 <jmoreno@procibernetica.com>
2019-09-25 12:27:25 -0500

    refactor code in get_delta_time file

--- app/services/alarms/accumulators/get_delta_time.rb
+++ app/services/alarms/accumulators/get_delta_time.rb
@@ -6,11 +6,16 @@ class Alarms::Accumulators::GetDeltaTime
   def call(input)
     current_accumulator_time = accumulator_time(input[:current_accumulator])
     last_accumulator_time = accumulator_time(input[:last_accumulator])
+    delta_time = { delta_time: delta_time(current_accumulator_time, last_accumulator_time) }
 
-    input.merge(delta_time: (current_accumulator_time - last_accumulator_time) / 60)
+    input.merge(delta_time)
   end
 
   private
+
+  def delta_time(current_accumulator_time, last_accumulator_time)
+    (current_accumulator_time - last_accumulator_time) / 60
+  end
 
   def accumulator_time(accumulator)
     Time.at accumulator.uplink.time.to_i
