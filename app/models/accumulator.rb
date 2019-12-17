c0d1cf05ae06fe0f0aad0bb19064180ef06418c1
William Calderon <wecalderonc@unal.edu.co>
2019-12-17 11:01:55 -0500

    added is an overturning property to accumulators

--- app/models/accumulator.rb
+++ app/models/accumulator.rb
@@ -1,5 +1,6 @@
 require_relative 'base_model.rb'
 
 class Accumulator < BaseModel
+  property :is_an_overturning, type: Boolean, default: false
 end
 
--- spec/factories/accumulator.rb
+++ spec/factories/accumulator.rb
@@ -3,6 +3,7 @@ require 'faker'
 FactoryBot.define do
   factory :accumulator do
     value { Faker::Number.hexadecimal(digits: 8) }
+    is_an_overturning { false }
 
     association :uplink, factory: :uplink
   end
